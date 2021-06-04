f2fa5864b8c780ff2158dc490389b9c409a7cd42
Mark Lisé <mark@digitalspace.ca>
2021-06-04 13:26:28 -0700

    Update getters

--- readPark/index.js
+++ readPark/index.js
@@ -14,37 +14,40 @@ exports.handler = async (event, context) => {
         queryObj.ExpressionAttributeValues = {};
         queryObj.ExpressionAttributeValues[':pk'] = { S: 'park' };
         queryObj.KeyConditionExpression = 'pk =:pk';
+        const parksData = await runQuery(queryObj);
+        return sendResponse(200, parksData);
       } else if (event.queryStringParameters.facilities && event.queryStringParameters.park) {
         // Grab facilities for this park.
         queryObj.ExpressionAttributeValues = {};
         queryObj.ExpressionAttributeValues[':pk'] = { S: 'facility::' + event.queryStringParameters.park };
         queryObj.KeyConditionExpression = 'pk =:pk';
+        const facilityData = await runQuery(queryObj);
+        return sendResponse(200, facilityData);
       } else if (event.queryStringParameters.park) {
         // Get all the parks, no specific things
         queryObj.ExpressionAttributeValues = {};
         queryObj.ExpressionAttributeValues[':pk'] = { S: 'park' };
         queryObj.ExpressionAttributeValues[':sk'] = { S: event.queryStringParameters.park };
         queryObj.KeyConditionExpression = 'pk =:pk AND sk =:sk';
+        const parkData = await runQuery(queryObj);
+        return sendResponse(200, parkData);
       } else {
         return sendResponse(400, { msg: 'Invalid Request'});
       }
-
-      try {
-        console.log("queryObj:", queryObj);
-        const data = await dynamodb.query(queryObj).promise();
-        console.log("data:", data);
-        var unMarshalled = data.Items.map(item => {
-          return AWS.DynamoDB.Converter.unmarshall(item);
-        });
-        console.log(unMarshalled);
-        return sendResponse(200, unMarshalled);
-      } catch (err) {
-        console.log(err);
-        return err;
-      }
     } catch (err) {
       console.log(err);
+      return sendResponse(400, err);
     }
+}
+
+const runQuery = async function (query) {
+  const data = await dynamodb.query(query).promise();
+  console.log("data:", data);
+  var unMarshalled = data.Items.map(item => {
+    return AWS.DynamoDB.Converter.unmarshall(item);
+  });
+  console.log(unMarshalled);
+  return unMarshalled;
 }
 
 var sendResponse = function (code, data) {
