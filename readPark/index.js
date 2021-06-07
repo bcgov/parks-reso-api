f7112c35ac2e3c72a5fb9a377916dd16c47f3607
Mark Lisé <mark@digitalspace.ca>
2021-06-07 10:56:30 -0700

    Update cors

--- readPark/index.js
+++ readPark/index.js
@@ -64,9 +64,9 @@ var sendResponse = function (code, data, context) {
       statusCode: code,
       headers: {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Headers" : "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "Access-Control-Allow-Origin" : "'*'",
-        "Access-Control-Allow-Methods": "'OPTIONS,GET'"
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Methods": "OPTIONS,GET"
       },
       body: JSON.stringify(data)
     };
