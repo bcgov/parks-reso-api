daf0109bf97f2edd4800c11bf9b90d5fd67ce05d
Max Wardle <max.wardle@gov.bc.ca>
2021-06-14 10:13:06 -0700

    added migration tool

--- writePass/index.js
+++ writePass/index.js
@@ -83,11 +83,9 @@ exports.handler = async (event, context) => {
         }'", (error, stdout, stderr) => {
           if (error) {
               console.log(`error: ${error.message}`);
-              return;
           }
           if (stderr) {
               console.log(`stderr: ${stderr}`);
-              return;
           }
           console.log(`stdout: ${stdout}`);
       });
