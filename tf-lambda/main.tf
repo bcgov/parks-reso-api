86884514d20155ec2f1cc024fdde8ae3a230d250
Morgan Thompson <morgan.thompson@gov.bc.ca>
2021-07-15 14:16:09 -0700

    add CORS config for lambda invocations

--- tf-lambda/main.tf
+++ tf-lambda/main.tf
@@ -147,6 +147,52 @@ resource "aws_lambda_permission" "writePermission" {
    source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/POST/park"
 }
 
+//CORS
+resource "aws_api_gateway_method" "park_options_method" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.readResource.id
+    http_method   = "OPTIONS"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "park_options_200" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.readResource.id
+    http_method   = "${aws_api_gateway_method.park_options_method.http_method}"
+    status_code   = "200"
+    response_models = {
+        "application/json" = "Empty"
+    }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+    depends_on = [aws_api_gateway_method.park_options_method]
+}
+
+resource "aws_api_gateway_integration" "park_options_integration" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.readResource.id
+    http_method   = aws_api_gateway_method.park_options_method.http_method
+    type          = "MOCK"
+    depends_on = [aws_api_gateway_method.park_options_method]
+}
+
+resource "aws_api_gateway_integration_response" "park_options_integration_response" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.readResource.id
+    http_method   = aws_api_gateway_method.park_options_method.http_method
+
+    status_code   = aws_api_gateway_method_response.park_options_200.status_code
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
+    depends_on = [aws_api_gateway_method_response.park_options_200]
+}
+
 // Tells us what our api endpoint is in the CLI
 output "base_url" {
   value = aws_api_gateway_deployment.apideploy.invoke_url
--- tf-lambda/settings.tf
+++ tf-lambda/settings.tf
@@ -107,3 +107,49 @@ resource "aws_lambda_permission" "writeConfigPermission" {
    principal     = "apigateway.amazonaws.com"
    source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/POST/config"
 }
+
+//CORS
+resource "aws_api_gateway_method" "config_options_method" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.configResource.id
+    http_method   = "OPTIONS"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "config_options_200" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.configResource.id
+    http_method   = aws_api_gateway_method.config_options_method.http_method
+    status_code   = "200"
+    response_models = {
+        "application/json" = "Empty"
+    }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+    depends_on = [aws_api_gateway_method.config_options_method]
+}
+
+resource "aws_api_gateway_integration" "config_options_integration" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.configResource.id
+    http_method   = aws_api_gateway_method.config_options_method.http_method
+    type          = "MOCK"
+    depends_on = [aws_api_gateway_method.config_options_method]
+}
+
+resource "aws_api_gateway_integration_response" "config_options_integration_response" {
+    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+    resource_id   = aws_api_gateway_resource.configResource.id
+    http_method   = aws_api_gateway_method.config_options_method.http_method
+
+    status_code   = aws_api_gateway_method_response.config_options_200.status_code
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
+    depends_on = [aws_api_gateway_method_response.config_options_200]
+}
