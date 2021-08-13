f59b9b5fbd9fb3376f8ec8a4466a0ef1e1461ee1
Mark Lisé <marklise@users.noreply.github.com>
2021-08-13 12:31:43 -0700

    Update main.tf

--- tf-lambda/main.tf
+++ tf-lambda/main.tf
@@ -159,7 +159,7 @@ resource "aws_api_gateway_method" "park_options_method" {
 resource "aws_api_gateway_method_response" "park_options_200" {
     rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
     resource_id   = aws_api_gateway_resource.readResource.id
-    http_method   = "${aws_api_gateway_method.park_options_method.http_method}"
+    http_method   = aws_api_gateway_method.park_options_method.http_method
     status_code   = "200"
     response_models = {
         "application/json" = "Empty"
