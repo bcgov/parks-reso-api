3eb02365c1da423b140fe417e2007f2859d12c86
Mark Lisé <marklise@users.noreply.github.com>
2021-08-13 12:29:17 -0700

    Update expiryJob.tf

--- tf-lambda/expiryJob.tf
+++ tf-lambda/expiryJob.tf
@@ -7,7 +7,7 @@ data "archive_file" "checkExpiryZip" {
 resource "aws_lambda_function" "check_expiry" {
    function_name = "checkExpiry"
    filename = "checkExpiry.zip"
-   source_code_hash = "data.archive_file.checkExpiryZip.output_base64sha256"
+   source_code_hash = data.archive_file.checkExpiryZip.output_base64sha256
 
    handler = "index.handler"
    runtime = "nodejs12.x"
@@ -28,15 +28,15 @@ resource "aws_cloudwatch_event_rule" "every_morning_at_12am" {
 }
 
 resource "aws_cloudwatch_event_target" "check_expiry_every_morning_at_12am" {
-    rule = "aws_cloudwatch_event_rule.every_morning_at_12am.name"
+    rule = aws_cloudwatch_event_rule.every_morning_at_12am.name
     target_id = "check_expiry"
-    arn = "aws_lambda_function.check_expiry.arn"
+    arn = aws_lambda_function.check_expiry.arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_expiry" {
     statement_id = "AllowExecutionFromCloudWatch"
     action = "lambda:InvokeFunction"
-    function_name = "aws_lambda_function.check_expiry.function_name"
+    function_name = aws_lambda_function.check_expiry.function_name
     principal = "events.amazonaws.com"
-    source_arn = "aws_cloudwatch_event_rule.every_morning_at_12am.arn"
+    source_arn = aws_cloudwatch_event_rule.every_morning_at_12am.arn
 }
