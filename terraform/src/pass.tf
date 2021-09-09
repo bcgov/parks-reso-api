61e074be44628e2f6118277cd00b463ecf22d576
Daniel Truong <daniel@dtsoftware.ca>
2021-09-09 16:25:04 -0700

    Fixing issues with missing s3 bucket

--- terraform/src/pass.tf
+++ terraform/src/pass.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "readPassLambda" {
    function_name = "readPass"
 
    # This method is for deploying things outside of TF.
-   s3_bucket = var.s3_bucket
+   s3_bucket = "${var.s3_bucket}-${var.target_env}"
    s3_key    = "${var.app_version}/readPass.zip"
 
    handler = "index.handler"
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "writePassLambda" {
    function_name = "writePass"
 
    # This method is for deploying things outside of TF.
-   s3_bucket = var.s3_bucket
+   s3_bucket = "${var.s3_bucket}-${var.target_env}"
    s3_key    = "${var.app_version}/writePass.zip"
 
    handler = "index.handler"
@@ -66,7 +66,7 @@ resource "aws_lambda_function" "deletePassLambda" {
    function_name = "deletePass"
 
    # This method is for deploying things outside of TF.
-   s3_bucket = var.s3_bucket
+   s3_bucket = "${var.s3_bucket}-${var.target_env}"
    s3_key    = "${var.app_version}/deletePass.zip"
 
    handler = "index.handler"
