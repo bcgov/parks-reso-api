1b48ad12cda1ace669519be955ce77405bec8d83
Daniel Truong <daniel@dtsoftware.ca>
2021-11-19 15:29:06 -0800

    Formatting date

--- lambda/checkExpiry/index.js
+++ lambda/checkExpiry/index.js
@@ -2,62 +2,63 @@ const { runQuery, setStatus } = require('../dynamoUtil');
 const { sendResponse } = require('../responseUtil');
 
 exports.handler = async (event, context) => {
-    console.log('check expiry', event);
+  console.log('check expiry', event);
 
-    let queryObj = {
-      TableName: process.env.TABLE_NAME
-    };
+  let queryObj = {
+    TableName: process.env.TABLE_NAME
+  };
 
-    // Look for today's expiries
-    let yesterdaysDate = new Date();
-    yesterdaysDate.setDate(yesterdaysDate.getDate()-1);
+  // Look for today's expiries
+  let yd = new Date();
+  yd.setDate(yd.getDate() - 1);
+  const yesterdaysDate = formatDate(yd);
 
-    try {
-      queryObj.ExpressionAttributeValues = {};
-      queryObj.ExpressionAttributeValues[':pk'] = { S: 'park' };
-      queryObj.KeyConditionExpression = 'pk =:pk';
-      console.log("queryObj:", queryObj);
+  try {
+    queryObj.ExpressionAttributeValues = {};
+    queryObj.ExpressionAttributeValues[':pk'] = { S: 'park' };
+    queryObj.KeyConditionExpression = 'pk =:pk';
+    console.log("queryObj:", queryObj);
 
-      const parkData = await runQuery(queryObj);
+    const parkData = await runQuery(queryObj);
 
-      for(let i=0;i < parkData.length;i++) {
-        let passQuery = {
-          TableName: process.env.TABLE_NAME
-        };
-        passQuery.ExpressionAttributeNames = {
-          '#dateselector': 'date'
-        };
-        passQuery.ExpressionAttributeValues = {};
-        passQuery.ExpressionAttributeValues[':pk'] = { S: 'pass::' + parkData[i].sk };
-        passQuery.ExpressionAttributeValues[':yesterdaysDate'] = { S: yesterdaysDate };
-        passQuery.ExpressionAttributeValues[':activeStatus'] = { S: 'active' };
-        passQuery.KeyConditionExpression = 'pk =:pk';
-        passQuery.FilterExpression = 'begins_with(#dateselector, :yesterdaysDate) AND passStatus =:activeStatus';
+    for (let i = 0; i < parkData.length; i++) {
+      let passQuery = {
+        TableName: process.env.TABLE_NAME
+      };
+      passQuery.ExpressionAttributeNames = {
+        '#dateselector': 'date'
+      };
+      passQuery.ExpressionAttributeValues = {};
+      passQuery.ExpressionAttributeValues[':pk'] = { S: 'pass::' + parkData[i].sk };
+      passQuery.ExpressionAttributeValues[':yesterdaysDate'] = { S: yesterdaysDate };
+      passQuery.ExpressionAttributeValues[':activeStatus'] = { S: 'active' };
+      passQuery.KeyConditionExpression = 'pk =:pk';
+      passQuery.FilterExpression = 'begins_with(#dateselector, :yesterdaysDate) AND passStatus =:activeStatus';
 
-        console.log("passQuery:", passQuery);
-        const passData = await runQuery(passQuery);
-        console.log("passData:", passData);
+      console.log("passQuery:", passQuery);
+      const passData = await runQuery(passQuery);
+      console.log("passData:", passData);
 
-        await setStatus(passData, 'expired');
-      }
-      return sendResponse(200, {}, context);
-    } catch (err) {
-      console.log(err);
-      return sendResponse(200, {msg: 'Activation Check Complete'}, context);
+      await setStatus(passData, 'expired');
     }
+    return sendResponse(200, {}, context);
+  } catch (err) {
+    console.log(err);
+    return sendResponse(200, { msg: 'Activation Check Complete' }, context);
+  }
 }
 
 
 
 function formatDate(d) {
   let month = '' + (d.getMonth() + 1),
-      day = '' + (d.getDate() - 1), // We need yesterday's date to check for moving active -> expired
-      year = d.getFullYear();
+    day = '' + (d.getDate() - 1), // We need yesterday's date to check for moving active -> expired
+    year = d.getFullYear();
 
   if (month.length < 2)
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
 
   return [year, month, day].join('-');
 }
