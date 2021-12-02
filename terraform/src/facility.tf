d225a9708489d32c468334ed1d286737a97fa61a
Cole Maclean <c.maclean@oxd.com>
2021-12-02 15:31:07 -0800

    NOBUG: update facility intergration name

--- terraform/src/facility.tf
+++ terraform/src/facility.tf
@@ -88,7 +88,7 @@ resource "aws_api_gateway_integration" "writeFacilityIntegration" {
   uri                     = aws_lambda_function.writeFacilityLambda.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "putIntegration" {
+resource "aws_api_gateway_integration" "putFacilityIntegration" {
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
   resource_id = aws_api_gateway_resource.readResource.id
   http_method = aws_api_gateway_method.putFacilityMethod.http_method
