719444fddc6a8dc0dcd071ad6b4e49368b556c48
Cole Maclean <c.maclean@oxd.com>
2021-12-08 14:31:11 -0800

    BRS-161: update pass activation logic
    Run the cron hourly, and set passes to active depending on pass type
    and facility opening hour:
    
    AM - activates at facility opening hour (default pulled from config)
    PM - activates at 12pm
    DAY - activates at facility opening hour (default pulled from config)
    
    Expiry runs hourly but doesn't depend on the facility:
    
    AM - 12pm
    PM - 12am
    DAY - 12am
    
    Hourly is a bit more resource intensive but means that we don't have
    to update the schedule around DST changes (when the offset from PT to
    UTC will be different).
    
    Status changes will only happen the same day (or day after for PM/DAY)
    expiry, so should be resistant to a few cron failures, but not a longer
    outage.

---
+++ __tests__/checkActivation.test.js
@@ -0,0 +1,324 @@
+const MockDate = require('mockdate');
+const { formatISO } = require('date-fns');
+const AWS = require('aws-sdk');
+const { DocumentClient } = require('aws-sdk/clients/dynamodb');
+
+const checkActivation = require('../lambda/checkActivation/index');
+
+const REGION = process.env.AWS_REGION || 'local-env';
+const ENDPOINT = 'http://localhost:8000';
+const TABLE_NAME = process.env.TABLE_NAME || 'parksreso';
+
+let dynamoDb;
+let docClient;
+
+async function setupDb() {
+  dynamoDb = new AWS.DynamoDB({
+    region: REGION,
+    endpoint: ENDPOINT
+  });
+  docClient = new DocumentClient({
+    region: REGION,
+    endpoint: ENDPOINT,
+    convertEmptyValues: true
+  });
+  try {
+    await dynamoDb
+      .createTable({
+        TableName: TABLE_NAME,
+        KeySchema: [
+          {
+            AttributeName: 'pk',
+            KeyType: 'HASH'
+          },
+          {
+            AttributeName: 'sk',
+            KeyType: 'RANGE'
+          }
+        ],
+        AttributeDefinitions: [
+          {
+            AttributeName: 'pk',
+            AttributeType: 'S'
+          },
+          {
+            AttributeName: 'sk',
+            AttributeType: 'S'
+          }
+        ],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1,
+          WriteCapacityUnits: 1
+        }
+      })
+      .promise();
+  } catch (err) {
+    console.log(err);
+  }
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: {
+        pk: 'config',
+        sk: 'config',
+        BOOKING_OPENING_HOUR: 7
+      }
+    })
+    .promise();
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: {
+        pk: 'park',
+        sk: 'Test Park',
+        name: 'Test Park',
+        description: '',
+        bcParksLink: '',
+        status: 'open',
+        visible: true
+      }
+    })
+    .promise();
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: {
+        pk: 'facility::Test Park',
+        sk: 'Parking Lot A',
+        name: 'Parking Lot A',
+        description: '',
+        bcParksLink: '',
+        status: 'open',
+        visible: true,
+        type: 'parking',
+        reservations: {},
+        bookingOpeningHour: null,
+        bookingDaysAhead: null
+      }
+    })
+    .promise();
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: {
+        pk: 'facility::Test Park',
+        sk: 'Parking Lot B',
+        name: 'Parking Lot B',
+        description: '',
+        bcParksLink: '',
+        status: 'open',
+        visible: true,
+        type: 'parking',
+        reservations: {},
+        bookingOpeningHour: 10,
+        bookingDaysAhead: null
+      }
+    })
+    .promise();
+}
+
+describe('checkActivationHandler', () => {
+  beforeAll(() => {
+    return setupDb();
+  });
+
+  test('should not update old passes', async () => {
+    const oldDate = new Date();
+    oldDate.setDate(oldDate.getDate() - 5);
+
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: '123456700',
+          facilityName: 'Parking Lot A',
+          type: 'DAY',
+          registrationNumber: '123456700',
+          passStatus: 'reserved',
+          date: formatISO(oldDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T11:01:58.135Z'));
+    await checkActivation.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: '123456700'
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('reserved');
+  });
+
+  test.each([['AM', '123456702'], ['DAY', '123456703']])('should set %s passes with default opening hour to active', async (passType, sk) => {
+    const passDate = new Date('2021-12-08T19:01:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: sk,
+          facilityName: 'Parking Lot A',
+          type: passType,
+          registrationNumber: sk,
+          passStatus: 'reserved',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T19:01:58.135Z'));
+    await checkActivation.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: sk
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('active');
+  });
+
+  test.each([['AM', '123456704'], ['DAY', '123456705']])('should leave %s passes inactive before custom opening hour', async (passType, sk) => {
+    const passDate = new Date('2021-12-08T19:01:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: sk,
+          facilityName: 'Parking Lot B',
+          type: passType,
+          registrationNumber: sk,
+          passStatus: 'reserved',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T17:01:58.135Z'));
+    await checkActivation.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: sk,
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('reserved');
+  });
+
+  test.each([['AM', '123456706'], ['DAY', '123456707']])('should set %s passes to active after custom opening hour', async (passType, sk) => {
+    const passDate = new Date('2021-12-08T19:01:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: sk,
+          facilityName: 'Parking Lot B',
+          type: passType,
+          registrationNumber: sk,
+          passStatus: 'reserved',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T18:00:00.00Z'));
+    await checkActivation.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: sk,
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('active');
+  });
+
+  test('should leave PM passes before 12:00 inactive', async () => {
+    const passDate = new Date('2021-12-08T19:01:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: '123456708',
+          facilityName: 'Parking Lot A',
+          type: 'PM',
+          registrationNumber: '123456708',
+          passStatus: 'reserved',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T19:59:59.999Z'));
+    await checkActivation.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: '123456708'
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('reserved');
+  });
+
+  test('should set PM passes after 12:00 to active', async () => {
+    const passDate = new Date('2021-12-08T19:01:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: '123456709',
+          facilityName: 'Parking Lot A',
+          type: 'PM',
+          registrationNumber: '123456709',
+          passStatus: 'reserved',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T22:01:58.135Z'));
+    await checkActivation.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: '123456709'
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('active');
+  });
+});
---
+++ __tests__/checkExpiry.test.js
@@ -0,0 +1,264 @@
+const MockDate = require('mockdate');
+const { formatISO } = require('date-fns');
+const AWS = require('aws-sdk');
+const { DocumentClient } = require('aws-sdk/clients/dynamodb');
+
+const checkExpiry = require('../lambda/checkExpiry/index');
+
+const REGION = process.env.AWS_REGION || 'local-env';
+const ENDPOINT = 'http://localhost:8000';
+const TABLE_NAME = process.env.TABLE_NAME || 'parksreso';
+
+let dynamoDb;
+let docClient;
+
+async function setupDb() {
+  dynamoDb = new AWS.DynamoDB({
+    region: REGION,
+    endpoint: ENDPOINT
+  });
+  docClient = new DocumentClient({
+    region: REGION,
+    endpoint: ENDPOINT,
+    convertEmptyValues: true
+  });
+  try {
+    await dynamoDb
+      .createTable({
+        TableName: TABLE_NAME,
+        KeySchema: [
+          {
+            AttributeName: 'pk',
+            KeyType: 'HASH'
+          },
+          {
+            AttributeName: 'sk',
+            KeyType: 'RANGE'
+          }
+        ],
+        AttributeDefinitions: [
+          {
+            AttributeName: 'pk',
+            AttributeType: 'S'
+          },
+          {
+            AttributeName: 'sk',
+            AttributeType: 'S'
+          }
+        ],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 1,
+          WriteCapacityUnits: 1
+        }
+      })
+      .promise();
+  } catch (err) {
+    console.log(err);
+  }
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: {
+        pk: 'park',
+        sk: 'Test Park',
+        name: 'Test Park',
+        description: '',
+        bcParksLink: '',
+        status: 'open',
+        visible: true
+      }
+    })
+    .promise();
+  await docClient
+    .put({
+      TableName: TABLE_NAME,
+      Item: {
+        pk: 'facility::Test Park',
+        sk: 'Parking Lot A',
+        name: 'Parking Lot A',
+        description: '',
+        bcParksLink: '',
+        status: 'open',
+        visible: true,
+        type: 'parking',
+        reservations: {},
+        bookingOpeningHour: null,
+        bookingDaysAhead: null
+      }
+    })
+    .promise();
+}
+
+describe('checkExpiryHandler', () => {
+  beforeAll(() => {
+    return setupDb();
+  });
+
+  test('should not update old passes', async () => {
+    const oldDate = new Date();
+    oldDate.setDate(oldDate.getDate() - 5);
+
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: '123456710',
+          facilityName: 'Parking Lot A',
+          type: 'DAY',
+          registrationNumber: '123456710',
+          passStatus: 'active',
+          date: formatISO(oldDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T11:01:30.135Z'));
+    await checkExpiry.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: '123456710'
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('active');
+  });
+
+  test.each([['PM', '123456711'], ['DAY', '123456712']])('should set %s passes from yesterday to expired', async (passType, sk) => {
+    const passDate = new Date('2021-12-07T11:07:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: sk,
+          facilityName: 'Parking Lot A',
+          type: passType,
+          registrationNumber: sk,
+          passStatus: 'active',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T08:01:58.135Z'));
+    await checkExpiry.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: sk
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('expired');
+  });
+
+  test.each([['PM', '123456713'], ['DAY', '123456714']])('should not set %s passes from today to expired', async (passType, sk) => {
+    const passDate = new Date('2021-12-08T11:02:43.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: sk,
+          facilityName: 'Parking Lot A',
+          type: passType,
+          registrationNumber: sk,
+          passStatus: 'active',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T19:01:58.135Z'));
+    await checkExpiry.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: sk
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('active');
+  });
+
+  test('should set AM passes to expired after 12:00', async () => {
+    const passDate = new Date('2021-12-08T11:01:02.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: '123456715',
+          facilityName: 'Parking Lot A',
+          type: 'AM',
+          registrationNumber: '123456715',
+          passStatus: 'active',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T20:00:00.001Z'));
+    await checkExpiry.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: '123456715'
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('expired');
+  });
+
+  test('should set not AM passes to expired before 12:00', async () => {
+    const passDate = new Date('2021-12-08T11:01:58.135Z');
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: {
+          pk: 'pass::Test Park',
+          sk: '123456716',
+          facilityName: 'Parking Lot A',
+          type: 'AM',
+          registrationNumber: '123456716',
+          passStatus: 'active',
+          date: formatISO(passDate, { representation: 'date' })
+        }
+      })
+      .promise();
+
+    MockDate.set(new Date('2021-12-08T19:59:59.999Z'));
+    await checkExpiry.handler(null, {});
+    MockDate.reset();
+
+    const result = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          pk: 'pass::Test Park',
+          sk: '123456716'
+        }
+      })
+      .promise();
+    expect(result.Item.passStatus).toBe('active');
+  });
+
+});
