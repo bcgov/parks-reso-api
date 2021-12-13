bf353ece924043b63b9075e5163fbeeaeb79df95
Mark Lisé <marklise@users.noreply.github.com>
2021-12-13 11:20:07 -0800

    BRS-249: Data Backups.

--- terraform/src/db.tf
+++ terraform/src/db.tf
@@ -5,6 +5,10 @@ resource "aws_dynamodb_table" "park_dup_table" {
   read_capacity  = 1
   write_capacity = 1
 
+  point_in_time_recovery {
+    enabled = true
+  }
+
   attribute {
     name = "pk"
     type = "S"
@@ -13,5 +17,23 @@ resource "aws_dynamodb_table" "park_dup_table" {
   attribute {
     name = "sk"
     type = "S"
+  }
+}
+
+resource "aws_backup_vault" "parksreso_backup_vault" {
+  name        = "backup_vault_for_parksreso"
+}
+
+resource "aws_backup_plan" "parksreso_backup" {
+  name = "parksreso_backup_plan"
+
+  rule {
+    rule_name         = "parksreso_backup_rule"
+    target_vault_name = aws_backup_vault.parksreso_backup_vault.name
+    schedule          = "cron(0 12 * * ? *)"
+
+    lifecycle {
+      delete_after = 360
+    }
   }
 }
