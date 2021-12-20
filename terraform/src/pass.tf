d44762051fd64fd97a6ad63d3e7172decb75b79a
Mark Lisé <marklise@users.noreply.github.com>
2021-12-20 15:17:30 -0800

    BRS-292: CORs error reoccuring on email cancellation process (#57)

--- terraform/src/pass.tf
+++ terraform/src/pass.tf
@@ -209,7 +209,7 @@ resource "aws_api_gateway_integration_response" "pass_options_integration_respon
   status_code = aws_api_gateway_method_response.pass_options_200.status_code
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
   depends_on = [aws_api_gateway_method_response.pass_options_200]
