0403c1f55210dd3db95ff4a3da0663a6cd677159
Mark Lisé <mark@digitalspace.ca>
2022-02-15 00:43:09 +0000

    BRS-425: Update logic for pass expiry checks.

--- lambda/checkExpiry/index.js
+++ lambda/checkExpiry/index.js
@@ -1,10 +1,9 @@
-const { formatISO, subDays, getHours } = require('date-fns');
+const { compareAsc, addHours, endOfYesterday, startOfDay } = require('date-fns');
 const { utcToZonedTime } = require('date-fns-tz');
-
-const { runQuery, setStatus, getParks, TABLE_NAME } = require('../dynamoUtil');
+const { runQuery, setStatus, TABLE_NAME } = require('../dynamoUtil');
 const { sendResponse } = require('../responseUtil');
 
-const TIMEZONE = 'America/Vancouver';
+const timeZone = 'America/Vancouver';
 const ACTIVE_STATUS = 'active';
 const EXPIRED_STATUS = 'expired';
 const PASS_TYPE_EXPIRY_HOURS = {
@@ -14,28 +13,34 @@ const PASS_TYPE_EXPIRY_HOURS = {
 };
 
 exports.handler = async (event, context) => {
-  console.log('Event', event, context);
+  console.log('Check Expiry', event, context);
   try {
-    const utcNow = Date.now();
-    const localNow = utcToZonedTime(utcNow, TIMEZONE);
-    const localHour = getHours(localNow);
-    const yesterday = subDays(new Date(localNow), 1);
-    console.log(`UTC: ${utcNow}; local (${TIMEZONE}): ${localNow}; yesterday: ${yesterday}`);
+    const endOfYesterdayTime = endOfYesterday();
+    const currentTime = utcToZonedTime(new Date(), timeZone);
 
-    for (const passType in PASS_TYPE_EXPIRY_HOURS) {
-      const expiryHour = PASS_TYPE_EXPIRY_HOURS[passType];
-      if (localHour < expiryHour) {
-        console.log(`${passType} passes don't expire yet`);
-        continue;
+    let passesToChange = [];
+    const passes = await getActivePasses();
+    console.log("Active Passes:", passes);
+
+    for(pass of passes) {
+      const zonedPassTime = utcToZonedTime(pass.date, timeZone);
+      // If it's zoned date is before the end of yesterday, it's definitely expired (AM/PM/DAY)
+      if (compareAsc(zonedPassTime, endOfYesterdayTime) <= 0) {
+        console.log("Expiring:", pass);
+        passesToChange.push(pass);
       }
 
-      // If expiring at midnight, check yesterday's passes.
-      const expiryDate = expiryHour === 0 ? yesterday : localNow;
-      const parks = await getParks();
-      for (const park of parks) {
-        const passes = await getExpiredPasses(passType, expiryDate, park.sk);
-        await setStatus(passes, EXPIRED_STATUS);
+      // If AM, see if we're currently in the afternoon or later compared to the pass date's noon time.
+      const noonTime = addHours(startOfDay(zonedPassTime), PASS_TYPE_EXPIRY_HOURS.AM);
+      if (pass.type === 'AM' && compareAsc(currentTime, noonTime) > 0) {
+        console.log("Expiring:", pass);
+        passesToChange.push(pass);
       }
+    }
+
+    // Set passes => expired
+    if (passesToChange.length !== 0) {
+      await setStatus(passesToChange, EXPIRED_STATUS);
     }
 
     return sendResponse(200, {}, context);
@@ -46,26 +51,26 @@ exports.handler = async (event, context) => {
   }
 };
 
-async function getExpiredPasses(passType, passDate, parkSk) {
-  const dateSelector = formatISO(passDate, { representation: 'date' });
-
-  console.log(`Loading ${passType} passes on ${dateSelector} for ${parkSk}`);
+async function getActivePasses() {
+  console.log(`Loading passes`);
 
   const passesQuery = {
     TableName: TABLE_NAME,
-    KeyConditionExpression: 'pk = :pk',
-    ExpressionAttributeNames: {
-      '#dateselector': 'date',
-      '#passType': 'type'
-    },
+    KeyConditionExpression: 'passStatus = :activeStatus',
+    IndexName: 'passStatus-index',
     ExpressionAttributeValues: {
-      ':pk': { S: `pass::${parkSk}` },
-      ':activeDate': { S: dateSelector },
-      ':activeStatus': { S: ACTIVE_STATUS },
-      ':passType': { S: passType }
-    },
-    FilterExpression: 'begins_with(#dateselector, :activeDate) AND #passType = :passType AND passStatus = :activeStatus'
+      ':activeStatus': { S: ACTIVE_STATUS }
+    }
   };
 
-  return await runQuery(passesQuery);
+  // Grab all the results, don't skip any.
+  let results = [];
+  let passData;
+  do {
+    passData = await runQuery(passesQuery, true);
+    passData.data.forEach((item) => results.push(item));
+    passesQuery.ExclusiveStartKey  = passData.LastEvaluatedKey;
+  } while(typeof passData.LastEvaluatedKey !== "undefined");
+
+  return results;
 }
