f249c2c989e9abf5c776e3dbe5fcdf1ff8598c9f
Mike Olund <mike@oxd.com>
2022-06-03 07:25:04 -0700

    BRS-309: Updates to migration based on PR feedback

--- migrations/20220527134528-resCountRefactoring.js
+++ migrations/20220527134528-resCountRefactoring.js
@@ -2,64 +2,92 @@
 const AWS = require('aws-sdk');
 
 exports.up = async function (dbOptions) {
+  /*
+resCount Refactoring - this migration updates existing facilities to turn the 'reservations' column 
+(which was previously an array) into individual records.  This allows the system to exclude records 
+in the past.  
+
+To run locally: Ensure your dyanmodb-local db is running in a docker instance or similar.
+
+`yarn migrate up`
+
+To run in AWS environment: Configure AWS credentials to target AWS env.
+
+`yarn migrate up`
+*/
   const TABLE_NAME = process.env.TABLE_NAME || 'parksreso';
   const dynamodb = new AWS.DynamoDB(dbOptions);
 
-  // do a 'scan' query to get records where the pk starts with 'facility::'
-  let queryObj = {
+  // get all the parks
+  let parksQueryObj = {
     TableName: TABLE_NAME,
-    ConsistentRead: false,
-    FilterExpression: 'begins_with(#pk, :facilityPrefix)',
     ExpressionAttributeValues: {
-      ':facilityPrefix': {
-        S: 'facility::'
-      }
+      ':pk': { S: 'park' }
     },
-    ExpressionAttributeNames: {
-      '#pk': 'pk'
-    }
+    KeyConditionExpression: 'pk =:pk'
   };
-  const queryResponse = await dynamodb.scan(queryObj).promise();
+  const parks = await dynamodb.query(parksQueryObj).promise();
 
-  // loop through the facilities
-  for (const facility of queryResponse.Items) {
-    if (facility.reservations) {
+  // loop through the parks
+  for (let park of parks.Items) {
+    // get the facilities for each park
+    let facilitiesQueryObj = {
+      TableName: TABLE_NAME,
+      ExpressionAttributeValues: {
+        ':pk': { S: `facility::${park.sk.S}` }
+      },
+      KeyConditionExpression: 'pk =:pk'
+    };
+    const facilities = await dynamodb.query(facilitiesQueryObj).promise();
+
+    // loop through the facilities
+    for (const facility of facilities.Items) {
+      let rowsInserted = 0;
       const parkName = facility.pk.S.split('::')[1];
       const facilityName = facility.sk.S;
-      const reservations = facility.reservations.M;
+      console.log(`${parkName}::${facilityName}`);
 
-      for (const dateselector of Object.keys(reservations)) {
-        // only use dates in YYYY-MM-DD format (there is some bad data in the dev environment)
-        if (dateselector.length === 10 && dateselector.indexOf('-') > -1) {
-          // insert a rescount record for each park/facility/date
-          let insertResCount = {
-            TableName: TABLE_NAME,
-            ConditionExpression: 'attribute_not_exists(pk) AND attribute_not_exists(sk)'
-          };
-          insertResCount.Item = {};
-          insertResCount.Item['pk'] = { S: `rescount::${parkName}::${facilityName}` };
-          insertResCount.Item['sk'] = { S: dateselector };
-          insertResCount.Item['reservations'] = reservations[dateselector];
+      // rewrite the reservations array items from each facility as individual 'rescount' records
+      if (facility.reservations) {
+        const reservations = facility.reservations.M;
 
-          try {
-            console.log('putting rescount:', insertResCount);
-            const insertResponse = await dynamodb.putItem(insertResCount).promise();
-            console.log('insertResponse:', insertResponse);
-          } catch (e) {
-            console.log('rescount already exists', e);
+        for (const dateselector of Object.keys(reservations)) {
+          // only use dates in YYYY-MM-DD format (there is some irregular data in the dev environment)
+          if (dateselector.length === 10 && dateselector.indexOf('-') > -1) {
+            // insert a rescount record for each park/facility/date
+            let insertResCount = {
+              TableName: TABLE_NAME
+            };
+            insertResCount.Item = {};
+            insertResCount.Item['pk'] = { S: `rescount::${parkName}::${facilityName}` };
+            insertResCount.Item['sk'] = { S: dateselector };
+            insertResCount.Item['reservations'] = reservations[dateselector];
+
+            try {
+              await dynamodb.putItem(insertResCount).promise();
+              rowsInserted++;
+            } catch (e) {
+              console.log('error adding rescount', e);
+            }
           }
         }
-      }
 
-      // remove the reservations from the facility
-      facility.reservations = undefined;
-      const updateObj = {
-        TableName: TABLE_NAME,
-        Item: facility
-      };
-      console.log('putting facility:', updateObj);
-      const updateResponse = await dynamodb.putItem(updateObj).promise();
-      console.log('updateResponse:', updateResponse);
+        console.log(`  - ${rowsInserted} rows inserted`);
+
+        // remove the reservations from the facility
+        facility.reservations = undefined;
+        const updateObj = {
+          TableName: TABLE_NAME,
+          Item: facility
+        };
+
+        try {
+          await dynamodb.putItem(updateObj).promise();
+          console.log('  - reservations column removed');
+        } catch (e) {
+          console.log('error removing reservations column', e);
+        }
+      }
     }
   }
 };
