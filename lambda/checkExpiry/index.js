0963d98a71a5efc8ac440bb1d76e22a8cb6abee4
Cameron Pettit <71421099+cameronpettit@users.noreply.github.com>
2022-06-13 13:02:25 -0700

    BRS-658 fixing time zone issues in checkExpiry.js (#110)

--- lambda/checkExpiry/index.js
+++ lambda/checkExpiry/index.js
@@ -1,35 +1,41 @@
 const { compareAsc, addHours, endOfYesterday, startOfDay } = require('date-fns');
 const { utcToZonedTime } = require('date-fns-tz');
+const { DateTime } = require('luxon');
 const { getPassesByStatus,
-        setStatus,
-        ACTIVE_STATUS,
-        EXPIRED_STATUS,
-        PASS_TYPE_EXPIRY_HOURS,
-        PASS_TYPE_AM,
-        timeZone } = require('../dynamoUtil');
+  setStatus,
+  ACTIVE_STATUS,
+  EXPIRED_STATUS,
+  PASS_TYPE_EXPIRY_HOURS,
+  PASS_TYPE_AM,
+  TIMEZONE } = require('../dynamoUtil');
 const { sendResponse } = require('../responseUtil');
 
 exports.handler = async (event, context) => {
   console.log('Check Expiry', event, context);
+  console.log('Server Time Zone:',
+    Intl.DateTimeFormat().resolvedOptions().timeZone || 'undefined',
+    `(${DateTime.now().toISO()})`
+  );
   try {
-    const endOfYesterdayTime = endOfYesterday();
-    const currentTime = utcToZonedTime(new Date(), timeZone);
+    const currentPSTDateTime = DateTime.now().setZone(TIMEZONE);
+    const yesterdayEndPSTDateTime = currentPSTDateTime.minus({ days: 1 }).endOf('day');
 
     let passesToChange = [];
     const passes = await getPassesByStatus(ACTIVE_STATUS);
     console.log("Active Passes:", passes);
 
-    for(pass of passes) {
-      const zonedPassTime = utcToZonedTime(pass.date, timeZone);
-      // If it's zoned date is before the end of yesterday, it's definitely expired (AM/PM/DAY)
-      if (compareAsc(zonedPassTime, endOfYesterdayTime) <= 0) {
+    for (pass of passes) {
+      // NOTE: Pass dates are stored in UTC. 
+      // If pass date converted to PST is before the end of yesterday, it's definitely expire (AM/PM/DAY)
+      const passPSTDateTime = DateTime.fromISO(pass.date).setZone(TIMEZONE);
+      if (passPSTDateTime <= yesterdayEndPSTDateTime){
         console.log("Expiring:", pass);
         passesToChange.push(pass);
       }
 
       // If AM, see if we're currently in the afternoon or later compared to the pass date's noon time.
-      const noonTime = addHours(startOfDay(zonedPassTime), PASS_TYPE_EXPIRY_HOURS.AM);
-      if (pass.type === PASS_TYPE_AM && compareAsc(currentTime, noonTime) > 0) {
+      const passAMExpiryPSTDateTime = currentPSTDateTime.startOf('day').plus({hours: PASS_TYPE_EXPIRY_HOURS.AM});
+      if (pass.type === PASS_TYPE_AM && currentPSTDateTime >= passAMExpiryPSTDateTime){
         console.log("Expiring:", pass);
         passesToChange.push(pass);
       }
