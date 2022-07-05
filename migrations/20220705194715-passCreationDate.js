7e22a7cf913b7d4d637ef36db44a40e362a9515d
cameronpettit <cameron@digitalspace.ca>
2022-07-05 23:17:08 +0000

    BRS-690-2 replacing dynamodb scan with query

--- migrations/20220705194715-passCreationDate.js
+++ migrations/20220705194715-passCreationDate.js
@@ -1,5 +1,5 @@
 'use strict';
-const { runScan, TABLE_NAME, dynamodb } = require('../lambda/dynamoUtil');
+const { TABLE_NAME, dynamodb, runQuery } = require('../lambda/dynamoUtil');
 
 exports.up = async function (dbOptions) {
   await updateAllPasses();
@@ -7,26 +7,55 @@ exports.up = async function (dbOptions) {
 
 // add creationDate and isOverbooked to all passes. 
 async function updateAllPasses() {
-  const scanObj = {
+
+  console.log('Adding creationDate and isOverbooked attributes to all passes')
+
+  const parkQueryObj = {
     TableName: TABLE_NAME,
     ConsistentRead: true,
     ExpressionAttributeNames: {
       '#pk': 'pk'
     },
     ExpressionAttributeValues: {
-      ':pk': { S: 'pass::' }
+      ':pk': { S: 'park' }
     },
-    FilterExpression: 'begins_with(#pk, :pk)'
+    KeyConditionExpression: '#pk = :pk'
   };
 
   let errors = [];
   let completed = [];
 
+  // get all passes for every park
   try {
-    const passes = await runScan(scanObj);
+    const parks = await runQuery(parkQueryObj);
+
+    let passes = [];
+
+    for (const park of parks) {
+      const passPk = `pass::${park.sk}`;
+      const passQueryObj = {
+        TableName: TABLE_NAME,
+        ConsistentRead: true,
+        ExpressionAttributeNames: {
+          '#pk': 'pk'
+        },
+        ExpressionAttributeValues: {
+          ':pk': { S: passPk }
+        },
+        KeyConditionExpression: '#pk = :pk'
+      };
+
+      try {
+        const parkPasses = await runQuery(passQueryObj);
+        if (parkPasses.length > 0) {
+          passes = passes.concat(parkPasses);
+        }
+      } catch (err) {
+        console.log('ERROR:', err);
+      }
+    }
 
     for (const pass of passes) {
-
       const updateObj = {
         TableName: TABLE_NAME,
         Key: {
@@ -38,7 +67,7 @@ async function updateAllPasses() {
           '#creationDate': 'creationDate'
         },
         ExpressionAttributeValues: {
-          ':isOverbooked': { BOOL: false},
+          ':isOverbooked': { BOOL: false },
           ':creationDate': { S: pass.date }
         },
         UpdateExpression: 'set #isOverbooked = :isOverbooked, #creationDate = :creationDate',
@@ -60,7 +89,7 @@ async function updateAllPasses() {
   console.log(`Successfully updated ${completed.length} passes. \n`)
   process.stdout.write(`Failed to update ${errors.length}\n`);
   let firstTime = true;
-  for(const item of errors) {
+  for (const item of errors) {
     if (firstTime) {
       console.log("Failed Items:");
       firstTime = false;
