cb914226a2700e8e868749833ef2435ea183c472
cameronpettit <cameron@digitalspace.ca>
2022-07-14 18:29:33 +0000

    reservations -> reservation

--- terraform/src/reservations.tf
+++ terraform/src/reservations.tf
@@ -49,10 +49,10 @@ resource "aws_lambda_provisioned_concurrency_config" "readReservationLambda" {
 resource "aws_api_gateway_resource" "reservationResource" {
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
   parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
-  path_part   = "reservations"
+  path_part   = "reservation"
 }
 
-// Defines the HTTP GET /reservations API
+// Defines the HTTP GET /reservation API
 resource "aws_api_gateway_method" "readReservationMethod" {
   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
   resource_id   = aws_api_gateway_resource.reservationResource.id
@@ -65,7 +65,7 @@ resource "aws_lambda_permission" "readReservationPermission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.readReservationLambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/GET/reservations"
+  source_arn    = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/GET/reservation"
 }
 
 // Integrates the APIG to Lambda via POST method
