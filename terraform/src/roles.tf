5cad2f51966a10816906ae54b3ead8f3b05363aa
Mark Lisé <mark@digitalspace.ca>
2022-10-25 17:55:29 -0700

    NOBUG: Roles update.

--- terraform/src/roles.tf
+++ terraform/src/roles.tf
@@ -285,6 +285,45 @@ resource "aws_iam_role_policy" "park_reso_dynamodb_export" {
   EOF
 }
 
+resource "aws_iam_role_policy" "park_reso_dynamodb_export" {
+  name = "park_reso_dynamodb_export"
+  role = aws_iam_role.exportRoleInvokable.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": [
+            "dynamodb:BatchGet*",
+            "dynamodb:DescribeStream",
+            "dynamodb:DescribeTable",
+            "dynamodb:Get*",
+            "dynamodb:Query",
+            "dynamodb:Scan",
+            "dynamodb:BatchWrite*",
+            "dynamodb:CreateTable",
+            "dynamodb:Delete*",
+            "dynamodb:Update*",
+            "dynamodb:PutItem"
+          ],
+          "Resource": "${aws_dynamodb_table.park_dup_table.arn}"
+      },
+      {
+          "Effect": "Allow",
+          "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource": "arn:aws:logs:*:*:*"
+      }
+    ]
+  }
+  EOF
+}
+
 resource "aws_iam_role_policy" "park_reso_dynamodb_metric" {
   name = "park_reso_dynamodb_metric"
   role = aws_iam_role.metricRole.id
