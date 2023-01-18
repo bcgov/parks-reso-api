eebf8ec2c0db50213e26204cd6791e9021f25b27
Mark Lisé <mark@digitalspace.ca>
2023-01-18 10:01:10 -0800

    NOBUG: Update put pass statement id

--- terraform/src/pass.tf
+++ terraform/src/pass.tf
@@ -234,7 +234,7 @@ resource "aws_lambda_permission" "writePassPermission" {
 }
 
 resource "aws_lambda_permission" "putPassPermission" {
-  statement_id  = "AllowParksDayUsePassAPIInvoke"
+  statement_id  = "AllowParksDayUsePassPutAPIInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.writePassLambda.function_name
   principal     = "apigateway.amazonaws.com"
