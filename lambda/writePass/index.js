1dc959b437527cdaaeb40ea67dd1749543d9410f
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2023-11-27 19:25:24 +0000

    Update writepass to use constant from util file

--- lambda/writePass/index.js
+++ lambda/writePass/index.js
@@ -6,7 +6,8 @@ const {
   TIMEZONE,
   getFacility,
   getPark,
-  getConfig
+  getConfig,
+  PM_ACTIVATION_HOUR
 } = require('../dynamoUtil');
 const { sendResponse, checkWarmup } = require('../responseUtil');
 const { decodeJWT, resolvePermissions } = require('../permissionUtil');
@@ -20,7 +21,7 @@ const { generateRegistrationNumber } = require('../captchaUtil');
 
 // default opening/closing hours in 24h time
 const DEFAULT_AM_OPENING_HOUR = 7;
-const DEFAULT_PM_OPENING_HOUR = 13;
+const DEFAULT_PM_OPENING_HOUR = PM_ACTIVATION_HOUR;
 
 async function modifyPassCheckInStatus(pk, sk, checkedIn) {
   let updateParams = {
