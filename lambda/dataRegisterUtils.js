7d29ae0097d668cf1f0815a79232e9a3205351df
cameron <cameron@digitalspace.ca>
2023-11-29 10:56:36 +1400

    PDR-224: updating cron to search for status=established

--- lambda/dataRegisterUtils.js
+++ lambda/dataRegisterUtils.js
@@ -3,9 +3,10 @@ const { logger } = require('./logger');
 
 const DATA_REGISTRY_URL = process.env.DATA_REGISTRY_URL;
 const DATA_REGISTER_NAME_API_KEY = process.env.DATA_REGISTER_NAME_API_KEY;
+const ESTABLISHED_STATE = 'established'
 
 async function getCurrentNameData(identifier) {
-  const url = DATA_REGISTRY_URL + `/parks/${identifier}/name?status=current`
+  const url = DATA_REGISTRY_URL + `/parks/${identifier}/name?status=${ESTABLISHED_STATE}`
   try {
     const data = await axios({
       method: 'get',
@@ -18,7 +19,7 @@ async function getCurrentNameData(identifier) {
     })
     return data;
   } catch (err) {
-    logger.error(`Failed to get current name data for ${identifier}.`, err);
+    logger.error(`Failed to get established name data for ${identifier}.`, err);
     throw err;
   }
 }
@@ -30,7 +31,7 @@ async function getCurrentDisplayNameById(identifier) {
     let item = data?.data?.data?.items[0];
     return item?.displayName || null;
   } catch (err) {
-    logger.error('Failed to get current display name by id.', err);
+    logger.error('Failed to get established display name by id.', err);
     throw err;
   }
 }
