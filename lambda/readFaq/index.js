5e4ee6cb4f29d5e4380cb9c13279d7f54bb91b41
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2024-01-15 20:23:52 +0000

    Add FAQ endpoint with GET and PUT

---
+++ lambda/readFaq/index.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+const { runQuery, TABLE_NAME, visibleFilter } = require('../dynamoUtil');
+const { sendResponse, checkWarmup } = require('../responseUtil');
+const { decodeJWT, roleFilter, resolvePermissions } = require('../permissionUtil');
+const { logger } = require('../logger');
+
+exports.handler = async (event, context) => {
+  logger.info('Read FAQ', event);
+  if (checkWarmup(event)) {
+    return sendResponse(200, {});
+  }
+
+  let queryObj = {
+    TableName: TABLE_NAME
+  };
+
+  try {
+    
+    const token = await decodeJWT(event);
+    const permissionObject = resolvePermissions(token);
+    queryObj.ExpressionAttributeValues = {};
+    queryObj.ExpressionAttributeValues[':pk'] = { S: 'faq' };
+    queryObj.ExpressionAttributeValues[':sk'] = { S: 'faq' };
+    queryObj.KeyConditionExpression = 'pk =:pk AND sk =:sk';
+    let faq = await runQuery(queryObj);
+    return sendResponse(200, faq, context);
+
+  } catch (err) {
+    console.log(err);
+    logger.error(err);
+    return sendResponse(400, err, context);
+  }
+};
---
+++ lambda/writeFaq/index.js
@@ -0,0 +1,80 @@
+const AWS = require('aws-sdk');
+
+const { dynamodb, TABLE_NAME } = require('../dynamoUtil');
+const { sendResponse } = require('../responseUtil');
+const { decodeJWT, resolvePermissions, getParkAccess } = require('../permissionUtil');
+const { logger } = require('../logger');
+
+exports.handler = async (event, context) => {
+  if (!event || !event.headers) {
+    logger.info('Unauthorized');
+    return sendResponse(403, { msg: 'Unauthorized' }, context);
+  }
+
+  if (!new Set(['POST', 'PUT']).has(event.httpMethod)) {
+    logger.info('Not Implemented');
+    return sendResponse(405, { msg: 'Not Implemented' }, context);
+  }
+
+  const token = await decodeJWT(event);
+  const permissionObject = resolvePermissions(token);
+
+  if (permissionObject.isAuthenticated !== true) {
+    logger.info('Unauthorized');
+    return sendResponse(403, { msg: 'Unauthorized' }, context);
+  }
+
+  try {
+    logger.debug(event.body);
+    const obj = JSON.parse(event.body);
+
+    if (event.httpMethod === 'PUT') {
+        if (permissionObject.isAdmin) {
+            return await updateItem(obj);
+        }else {
+            logger.info('Unauthorized');
+            throw 'Unauthorized Access.';
+          }
+    } else { logger.info('Not Implemented');
+    return sendResponse(405, { msg: 'Not Implemented' }, context);
+  }
+
+  } catch (err) {
+    logger.error('err', err);
+    return sendResponse(400, err, context);
+  }
+};
+
+async function updateItem(obj, context) {
+  const { faq } = obj;
+
+  let updateParams = {
+    Key: {
+      pk: { S: 'faq' },
+      sk: { S: 'faq' }
+    },
+    UpdateExpression: 'set',
+    ExpressionAttributeValues: {},
+    ReturnValues: 'ALL_NEW',
+    TableName: TABLE_NAME,
+    ConditionExpression: 'attribute_exists(pk) AND attribute_exists(sk)'
+  };
+
+  if ('faq' in obj) {
+    updateParams.UpdateExpression += ' #text = :faq,';
+    updateParams.ExpressionAttributeValues[':faq'] = { S: faq };
+    updateParams.ExpressionAttributeNames = { '#text': 'text' };
+  }
+
+  updateParams.UpdateExpression = updateParams.UpdateExpression.slice(0, -1);
+  logger.debug('Updating FAQ:', updateParams);
+
+  try {
+    const { Attributes } = await dynamodb.updateItem(updateParams).promise();
+    logger.info('Results:', Attributes);
+    return sendResponse(200, AWS.DynamoDB.Converter.unmarshall(Attributes), context);
+  } catch (error) {
+    logger.error('Error updating item:', error);
+    return sendResponse(500, { error: 'Internal Server Error' }, context);
+  }
+}
