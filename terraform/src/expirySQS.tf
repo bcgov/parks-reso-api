e65ef8e2a2f0cc89da4a200142859b0fec524837
Mark Lisé <marklise@users.noreply.github.com>
2024-05-17 14:54:04 -0400

    Update expirySQS.tf
    Move to 18.x

--- terraform/src/expirySQS.tf
+++ terraform/src/expirySQS.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "expiry_sqs_processor" {
   source_code_hash = filebase64sha256("artifacts/expirySQSProcessor.zip")
 
   handler = "lambda/purgeExpired/index.handler"
-  runtime = "nodejs20.x"
+  runtime = "nodejs18.x"
   timeout = 300
   publish = "true"
 
