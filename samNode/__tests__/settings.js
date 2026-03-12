ddecb24f54a27ae994bb4bb220261e48794a350a
Mark Lisé <mark@digitalspace.ca>
2026-03-12 10:35:02 -0700

    (fix): UT for TZ adjustments.

--- samNode/__tests__/settings.js
+++ samNode/__tests__/settings.js
@@ -1,5 +1,5 @@
 const REGION = process.env.AWS_REGION || 'local-env';
-const ENDPOINT = 'http://172.17.0.2:8000';
+const ENDPOINT = process.env.DYNAMODB_ENDPOINT_URL || 'http://172.17.0.2:8000';
 const TABLE_NAME = process.env.TABLE_NAME || 'parksreso-tests';
 const TIMEZONE = 'America/Vancouver';
 
